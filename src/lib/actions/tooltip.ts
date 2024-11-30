d25b475ce71c477bc2f50c8ad0f2afc96ed2fae6
shynome <shynome@gmail.com>
2024-11-30 15:48:21 +0800

    tooltip

---
+++ src/lib/actions/tooltip.ts
@@ -0,0 +1,20 @@
+import type { Action } from 'svelte/action'
+import { Tooltip } from 'bootstrap'
+
+type Options = Partial<Tooltip.Options>
+
+export const tooltip: Action<HTMLElement, Options | undefined> = function tooltip(
+	node,
+	cfg: Options = {},
+) {
+	let inst = Tooltip.getOrCreateInstance(node, cfg)
+	return {
+		update: (cfg) => {
+			// @ts-ignore
+			inst.setContent({ '.tooltip-inner': cfg!.title })
+		},
+		destroy: () => {
+			inst.dispose()
+		},
+	}
+}
