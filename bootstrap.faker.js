15df5651cec0d9b8e7061b78ebb935a727691286
shynome <shynome@gmail.com>
2025-02-02 02:40:34 +0800

    支持 ssr

---
+++ bootstrap.faker.js
@@ -0,0 +1,5 @@
+export class Modal {}
+export class Tooltip {}
+export class Collapse {}
+export class Dropdown {}
+export class Offcanvas {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import path from 'path'
+
+const faker = path.resolve('./bootstrap.faker.js')
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		host: 'bootstrap.lo.remoon.net',
+	},
+	resolve: {
+		alias: [
+			{
+				find: 'bootstrap',
+				replacement: path.resolve('./node_modules/bootstrap'),
+				customResolver(source, importer, options) {
+					if (options.ssr) {
+						return faker
+					}
+				},
+			},
+		],
 	},
 })
